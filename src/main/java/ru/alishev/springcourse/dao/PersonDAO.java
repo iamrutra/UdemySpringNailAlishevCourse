b58c6c6ce1265a7634c6955e196b98a3348bf786
sh1lly <sh1llyxdd@gmail.com>
2024-04-28 22:17:21 +0200

    new commit

---
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,32 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<Person>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.get(id);
+    }
+
+
+}
