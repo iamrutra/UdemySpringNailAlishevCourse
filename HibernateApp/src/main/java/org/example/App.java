ef59850f6b2d1ce0c0b8baf1e26163533ccc8fb3
sh1lly <sh1llyxdd@gmail.com>
2024-05-01 12:53:57 +0200

    change derectory

---
+++ HibernateApp/src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+            session.beginTransaction();
+
+            session.createQuery("delete from Person where age < 30").executeUpdate();
+
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
+
+    }
+}
